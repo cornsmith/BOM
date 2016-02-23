b2315ea21c3c0c733932221852bfa0dd90fa6e19
cornsmith <corn.smith@outlook.com>
2016-02-23 20:39:29 +1100

    Add skip to corrupted zip files

--- Code/download_BOM.R
+++ Code/download_BOM.R
@@ -8,7 +8,7 @@ stations <- read.delim("stations.txt", as.is = TRUE)
 stations$StartYear <- as.integer(sapply(strsplit(stations$Start, " "), "[[", 2))
 stations$EndYear <- as.integer(sapply(strsplit(stations$End, " "), "[[", 2))
 # active stations
-stations <- subset(stations, StartYear < 2015 & EndYear == 2016)
+stations <- subset(stations, StartYear < 2014 & EndYear == 2016)
 
 # Download and process ----------------------------------------------------
 download_obs_file <- function(station, obs_code){
@@ -22,6 +22,8 @@ download_obs_file <- function(station, obs_code){
         
         # download and unzip
         download.file(ziplink, paste0(obs_code, "_", station, ".zip"), quiet = TRUE, mode = "wb")
+        
+        print(station)
     }
 }
 
@@ -29,18 +31,27 @@ process_file <- function(filename, years){
     temp_dir = "./temp"
     unzip(filename, exdir = temp_dir)
     
-    # read csv
-    df <- read.csv(dir(temp_dir, pattern = ".csv", full.names = TRUE))
-    df <- df[df$Year %in% years , 2:6]
+    datafilename <- dir(temp_dir, pattern = ".csv", full.names = TRUE)
     
+    if(length(datafilename) == 1){
+        # read csv
+        df <- read.csv(datafilename)
+        df <- df[df$Year %in% years, 2:6]
+    } else {
+        df <- 0
+    }
     # delete csv
     file.remove(dir(temp_dir, full.names = TRUE))
     
     return(df)
 }
 
-# example
+# Process -----------------------------------------------------------------
+# 136, 122, 123, 193
 lapply(stations$Site, download_obs_file, obs_code = 122)
-df <- lapply(dir(pattern = "136_([0-9]{4,5}).zip"), process_file, years = c(2015))
-df <- do.call("rbind", df)
-# df <- process_file(stations$Site[4], years = c(2015))
+df <- lapply(dir(pattern = "136_([0-9]{4,5}).zip"), process_file, years = c(2014, 2015))
+data <- do.call("rbind", df)
+data <- data[data$Year != 0, ]
+
+save(data, file = "BOM.RData")
+
